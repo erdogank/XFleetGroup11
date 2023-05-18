cd55541fa705050ef3df651f9c17a61828210338
erdogank <112961176+erdogank@users.noreply.github.com>
2023-05-18 23:33:42 +0300

    18.05

--- src/test/java/com/xfleet/runners/CukesRunner.java
+++ src/test/java/com/xfleet/runners/CukesRunner.java
@@ -20,7 +20,7 @@ import org.junit.runner.RunWith;
         glue = "com/xfleet/step_definitions",
 
         dryRun = false,
-        tags = "@smoke",
+        tags = "@XFLEET10-885",
         publish = true
 
 
